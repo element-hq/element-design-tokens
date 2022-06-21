02ba42d9ec02f90370a6cfc35a68d7312696636c
Doug <douglase@element.io>
2022-06-21 18:53:26 +0100

    Separate global colours and add contentsAndAvatars

--- formats/swiftColor.js
+++ formats/swiftColor.js
@@ -1,3 +1,5 @@
+const _ = require('style-dictionary/lib/utils/es6_.js')
+
 /**
  * This custom format creates an extension of the SwiftUI Color
  * class and adds all the color tokens as static variables so that
@@ -12,72 +14,119 @@
  * ```
  */
 module.exports = function({ dictionary, options }) {
+  let mobileColors = dictionary.allProperties.filter(token => {
+    return token.attributes.type === `mobile`
+  })
+  
+  let globalColors = dictionary.allProperties.filter(token => {
+    return token.attributes.type === `global`
+  })
+  
+  let contentAndAvatars = dictionary.tokens.color.contentAndAvatars.value
+  
   return `import SwiftUI
 
 // MARK: SwiftUI
 
 extension Color {
     public static let element = ElementColors()
+    public static let global = ElementColors.GlobalColors()
 }
 
 public struct ElementColors {\n` +
-  dictionary.allProperties.map(token => {
-    if (token.attributes.category === `color`) {
-      let value;
-      // if the token does not have a reference or has a darkValue
-      // use the colorset of the same name
-      // else use the reference name
-      if (options.outputReferences) {
-        // if it has a dark value -> use the colorset (all colors with darkValue have a colorset)
-        if (token.darkValue) {
-          value = `Color("${token.name}", bundle: Bundle.module)`;
-        // if it is a reference -> refer to the Color extension name
-        } else if (dictionary.usesReference(token.original.value)) {
-          const reference = dictionary.getReferences(token.original.value)[0];
-          value = `${reference.name}`
-        // default to using the colorset
-        } else {
-          value = `Color("${token.name}", bundle: Bundle.module)`
-        }
-      } else {
-        value = `Color("${token.name}", bundle: Bundle.module)`;
-      }
-      return `    public var ${token.name}: Color { ${value} }`
+  mobileColors.map(token => {
+    let value = swiftUIColor(token, dictionary, options)
+    return `    public var ${itemName(token, options)}: Color { ${value} }`
+  }).join(`\n`) + `
+    
+    public let contentAndAvatars: [Color] = [\n` +
+  contentAndAvatars.map(token => {
+    return `        .global.${token.value},`
+  }).join(`\n`) + `
+    ]
+    
+    public struct GlobalColors {\n` +
+  globalColors.map(token => {
+    let value = swiftUIColor(token, dictionary, options)
+    return `        public var ${itemName(token, options)}: Color { ${value} }`
+  }).join(`\n`) + `
     }
-  }).join(`\n`) +
-  `\n}
+}
 
 // MARK: UIKit
 
 extension UIColor {
     /// The colors from Compound, as dynamic colors that automatically update for light and dark mode.
     public static let element = ElementUIColors()
+    public static let global = ElementUIColors.GlobalColors()
 }
 
 @objcMembers public class ElementUIColors: NSObject {\n` +
-dictionary.allProperties.map(token => {
-  if (token.attributes.category === `color`) {
-    let value;
-    // if the token does not have a reference or has a darkValue
-    // use the colorset of the same name
-    // else use the reference name
-    if (options.outputReferences) {
-      // if it has a dark value -> use the colorset (all colors with darkValue have a colorset)
-      if (token.darkValue) {
-        value = `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`;
-      // if it is a reference -> refer to the Color extension name
-      } else if (dictionary.usesReference(token.original.value)) {
-        const reference = dictionary.getReferences(token.original.value)[0];
-        value = `${reference.name}`
-      // default to using the colorset
-      } else {
-        value = `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`
-      }
-    } else {
-      value = `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`;
+  mobileColors.map(token => {
+    let value = uiKitColor(token, dictionary, options);
+    return `    public var ${itemName(token, options)}: UIColor { ${value} }`
+  }).join(`\n`) + `
+    
+    public let contentAndAvatars: [UIColor] = [\n` +
+  contentAndAvatars.map(token => {
+    return `        .global.${token.value},`
+  }).join(`\n`) + `
+    ]
+    
+    @objcMembers public class GlobalColors: NSObject {\n` +
+  globalColors.map(token => {
+    let value = uiKitColor(token, dictionary, options);
+    return `        public var ${itemName(token, options)}: UIColor { ${value} }`
+  }).join(`\n`) + `
     }
-    return `    public var ${token.name}: UIColor { ${value} }`
+}\n`
+}
+
+/** The token's name without the category or type */
+function itemName(token, options) {
+  return _.camelCase( [options.prefix].concat(token.path.slice(2, token.path.length)).join(' ') )
+}
+
+/** The token's value as a Color for SwiftUI */
+function swiftUIColor(token, dictionary, options) {
+  // if the token does not have a reference or has a darkValue
+  // use the colorset of the same name
+  // else use the reference name
+  if (options.outputReferences) {
+    // if it has a dark value -> use the colorset (all colors with darkValue have a colorset)
+    if (token.darkValue) {
+      return `Color("${token.name}", bundle: Bundle.module)`;
+    // if it is a reference -> refer to the Color extension name
+    } else if (dictionary.usesReference(token.original.value)) {
+      const reference = dictionary.getReferences(token.original.value)[0];
+      return `.global.${itemName(reference, options)}`
+    // default to using the colorset
+    } else {
+      return `Color("${token.name}", bundle: Bundle.module)`
+    }
+  } else {
+    return `Color("${token.name}", bundle: Bundle.module)`;
   }
-}).join(`\n`) +
-`\n}\n`
+}
+
+/** The token's value as a UIColor for UIKit */
+function uiKitColor(token, dictionary, options) {
+  // if the token does not have a reference or has a darkValue
+  // use the colorset of the same name
+  // else use the reference name
+  if (options.outputReferences) {
+    // if it has a dark value -> use the colorset (all colors with darkValue have a colorset)
+    if (token.darkValue) {
+      return `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`;
+    // if it is a reference -> refer to the Color extension name
+    } else if (dictionary.usesReference(token.original.value)) {
+      const reference = dictionary.getReferences(token.original.value)[0];
+      return `.global.${itemName(reference, options)}`
+    // default to using the colorset
+    } else {
+      return `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`
+    }
+  } else {
+    return `UIColor(named: "${token.name}", in: Bundle.module, compatibleWith: nil)!`;
+  }
 }
--- types/ios/ElementColors.swift
+++ types/ios/ElementColors.swift
@@ -4,44 +4,60 @@ import SwiftUI
 
 extension Color {
     public static let element = ElementColors()
+    public static let global = ElementColors.GlobalColors()
 }
 
 public struct ElementColors {
-    public var globalAzure: Color { Color("globalAzure", bundle: Bundle.module) }
-    public var globalKiwi: Color { Color("globalKiwi", bundle: Bundle.module) }
-    public var globalGrape: Color { Color("globalGrape", bundle: Bundle.module) }
-    public var globalVerde: Color { Color("globalVerde", bundle: Bundle.module) }
-    public var globalPolly: Color { Color("globalPolly", bundle: Bundle.module) }
-    public var globalMelon: Color { Color("globalMelon", bundle: Bundle.module) }
-    public var globalElementGreen: Color { Color("globalElementGreen", bundle: Bundle.module) }
-    public var globalWhite: Color { Color("globalWhite", bundle: Bundle.module) }
-    public var globalVermillon: Color { Color("globalVermillon", bundle: Bundle.module) }
-    public var globalEms: Color { Color("globalEms", bundle: Bundle.module) }
-    public var globalAqua: Color { Color("globalAqua", bundle: Bundle.module) }
-    public var globalPrune: Color { Color("globalPrune", bundle: Bundle.module) }
-    public var globalLinks: Color { Color("globalLinks", bundle: Bundle.module) }
-    public var globalGrey25: Color { Color("globalGrey25", bundle: Bundle.module) }
-    public var globalGrey50: Color { Color("globalGrey50", bundle: Bundle.module) }
-    public var globalGrey100: Color { Color("globalGrey100", bundle: Bundle.module) }
-    public var globalGrey150: Color { Color("globalGrey150", bundle: Bundle.module) }
-    public var globalGrey200: Color { Color("globalGrey200", bundle: Bundle.module) }
-    public var globalGrey250: Color { Color("globalGrey250", bundle: Bundle.module) }
-    public var globalGrey300: Color { Color("globalGrey300", bundle: Bundle.module) }
-    public var globalGrey400: Color { Color("globalGrey400", bundle: Bundle.module) }
-    public var globalGrey450: Color { Color("globalGrey450", bundle: Bundle.module) }
-    public var globalBlack800: Color { Color("globalBlack800", bundle: Bundle.module) }
-    public var globalBlack900: Color { Color("globalBlack900", bundle: Bundle.module) }
-    public var globalBlack950: Color { Color("globalBlack950", bundle: Bundle.module) }
-    public var globalIce: Color { Color("globalIce", bundle: Bundle.module) }
-    public var accent: Color { globalElementGreen }
-    public var alert: Color { globalVermillon }
-    public var primaryContent: Color { Color("primaryContent", bundle: Bundle.module) }
-    public var secondaryContent: Color { Color("secondaryContent", bundle: Bundle.module) }
-    public var tertiaryContent: Color { Color("tertiaryContent", bundle: Bundle.module) }
-    public var quaternaryContent: Color { Color("quaternaryContent", bundle: Bundle.module) }
-    public var quinaryContent: Color { Color("quinaryContent", bundle: Bundle.module) }
-    public var system: Color { Color("system", bundle: Bundle.module) }
-    public var background: Color { Color("background", bundle: Bundle.module) }
+    public var accent: Color { .global.elementGreen }
+    public var alert: Color { .global.vermillon }
+    public var links: Color { .global.links }
+    public var primaryContent: Color { Color("mobilePrimaryContent", bundle: Bundle.module) }
+    public var secondaryContent: Color { Color("mobileSecondaryContent", bundle: Bundle.module) }
+    public var tertiaryContent: Color { Color("mobileTertiaryContent", bundle: Bundle.module) }
+    public var quaternaryContent: Color { Color("mobileQuaternaryContent", bundle: Bundle.module) }
+    public var quinaryContent: Color { Color("mobileQuinaryContent", bundle: Bundle.module) }
+    public var system: Color { Color("mobileSystem", bundle: Bundle.module) }
+    public var background: Color { Color("mobileBackground", bundle: Bundle.module) }
+    
+    public let contentAndAvatars: [Color] = [
+        .global.azure,
+        .global.grape,
+        .global.verde,
+        .global.polly,
+        .global.melon,
+        .global.aqua,
+        .global.prune,
+        .global.kiwi,
+    ]
+    
+    public struct GlobalColors {
+        public var azure: Color { Color("globalAzure", bundle: Bundle.module) }
+        public var kiwi: Color { Color("globalKiwi", bundle: Bundle.module) }
+        public var grape: Color { Color("globalGrape", bundle: Bundle.module) }
+        public var verde: Color { Color("globalVerde", bundle: Bundle.module) }
+        public var polly: Color { Color("globalPolly", bundle: Bundle.module) }
+        public var melon: Color { Color("globalMelon", bundle: Bundle.module) }
+        public var elementGreen: Color { Color("globalElementGreen", bundle: Bundle.module) }
+        public var white: Color { Color("globalWhite", bundle: Bundle.module) }
+        public var vermillon: Color { Color("globalVermillon", bundle: Bundle.module) }
+        public var ems: Color { Color("globalEms", bundle: Bundle.module) }
+        public var aqua: Color { Color("globalAqua", bundle: Bundle.module) }
+        public var prune: Color { Color("globalPrune", bundle: Bundle.module) }
+        public var links: Color { Color("globalLinks", bundle: Bundle.module) }
+        public var grey25: Color { Color("globalGrey25", bundle: Bundle.module) }
+        public var grey50: Color { Color("globalGrey50", bundle: Bundle.module) }
+        public var grey100: Color { Color("globalGrey100", bundle: Bundle.module) }
+        public var grey150: Color { Color("globalGrey150", bundle: Bundle.module) }
+        public var grey200: Color { Color("globalGrey200", bundle: Bundle.module) }
+        public var grey250: Color { Color("globalGrey250", bundle: Bundle.module) }
+        public var grey300: Color { Color("globalGrey300", bundle: Bundle.module) }
+        public var grey400: Color { Color("globalGrey400", bundle: Bundle.module) }
+        public var grey450: Color { Color("globalGrey450", bundle: Bundle.module) }
+        public var black800: Color { Color("globalBlack800", bundle: Bundle.module) }
+        public var black900: Color { Color("globalBlack900", bundle: Bundle.module) }
+        public var black950: Color { Color("globalBlack950", bundle: Bundle.module) }
+        public var ice: Color { Color("globalIce", bundle: Bundle.module) }
+    }
 }
 
 // MARK: UIKit
@@ -49,42 +65,58 @@ public struct ElementColors {
 extension UIColor {
     /// The colors from Compound, as dynamic colors that automatically update for light and dark mode.
     public static let element = ElementUIColors()
+    public static let global = ElementUIColors.GlobalColors()
 }
 
 @objcMembers public class ElementUIColors: NSObject {
-    public var globalAzure: UIColor { UIColor(named: "globalAzure", in: Bundle.module, compatibleWith: nil)! }
-    public var globalKiwi: UIColor { UIColor(named: "globalKiwi", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrape: UIColor { UIColor(named: "globalGrape", in: Bundle.module, compatibleWith: nil)! }
-    public var globalVerde: UIColor { UIColor(named: "globalVerde", in: Bundle.module, compatibleWith: nil)! }
-    public var globalPolly: UIColor { UIColor(named: "globalPolly", in: Bundle.module, compatibleWith: nil)! }
-    public var globalMelon: UIColor { UIColor(named: "globalMelon", in: Bundle.module, compatibleWith: nil)! }
-    public var globalElementGreen: UIColor { UIColor(named: "globalElementGreen", in: Bundle.module, compatibleWith: nil)! }
-    public var globalWhite: UIColor { UIColor(named: "globalWhite", in: Bundle.module, compatibleWith: nil)! }
-    public var globalVermillon: UIColor { UIColor(named: "globalVermillon", in: Bundle.module, compatibleWith: nil)! }
-    public var globalEms: UIColor { UIColor(named: "globalEms", in: Bundle.module, compatibleWith: nil)! }
-    public var globalAqua: UIColor { UIColor(named: "globalAqua", in: Bundle.module, compatibleWith: nil)! }
-    public var globalPrune: UIColor { UIColor(named: "globalPrune", in: Bundle.module, compatibleWith: nil)! }
-    public var globalLinks: UIColor { UIColor(named: "globalLinks", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey25: UIColor { UIColor(named: "globalGrey25", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey50: UIColor { UIColor(named: "globalGrey50", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey100: UIColor { UIColor(named: "globalGrey100", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey150: UIColor { UIColor(named: "globalGrey150", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey200: UIColor { UIColor(named: "globalGrey200", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey250: UIColor { UIColor(named: "globalGrey250", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey300: UIColor { UIColor(named: "globalGrey300", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey400: UIColor { UIColor(named: "globalGrey400", in: Bundle.module, compatibleWith: nil)! }
-    public var globalGrey450: UIColor { UIColor(named: "globalGrey450", in: Bundle.module, compatibleWith: nil)! }
-    public var globalBlack800: UIColor { UIColor(named: "globalBlack800", in: Bundle.module, compatibleWith: nil)! }
-    public var globalBlack900: UIColor { UIColor(named: "globalBlack900", in: Bundle.module, compatibleWith: nil)! }
-    public var globalBlack950: UIColor { UIColor(named: "globalBlack950", in: Bundle.module, compatibleWith: nil)! }
-    public var globalIce: UIColor { UIColor(named: "globalIce", in: Bundle.module, compatibleWith: nil)! }
-    public var accent: UIColor { globalElementGreen }
-    public var alert: UIColor { globalVermillon }
-    public var primaryContent: UIColor { UIColor(named: "primaryContent", in: Bundle.module, compatibleWith: nil)! }
-    public var secondaryContent: UIColor { UIColor(named: "secondaryContent", in: Bundle.module, compatibleWith: nil)! }
-    public var tertiaryContent: UIColor { UIColor(named: "tertiaryContent", in: Bundle.module, compatibleWith: nil)! }
-    public var quaternaryContent: UIColor { UIColor(named: "quaternaryContent", in: Bundle.module, compatibleWith: nil)! }
-    public var quinaryContent: UIColor { UIColor(named: "quinaryContent", in: Bundle.module, compatibleWith: nil)! }
-    public var system: UIColor { UIColor(named: "system", in: Bundle.module, compatibleWith: nil)! }
-    public var background: UIColor { UIColor(named: "background", in: Bundle.module, compatibleWith: nil)! }
+    public var accent: UIColor { .global.elementGreen }
+    public var alert: UIColor { .global.vermillon }
+    public var links: UIColor { .global.links }
+    public var primaryContent: UIColor { UIColor(named: "mobilePrimaryContent", in: Bundle.module, compatibleWith: nil)! }
+    public var secondaryContent: UIColor { UIColor(named: "mobileSecondaryContent", in: Bundle.module, compatibleWith: nil)! }
+    public var tertiaryContent: UIColor { UIColor(named: "mobileTertiaryContent", in: Bundle.module, compatibleWith: nil)! }
+    public var quaternaryContent: UIColor { UIColor(named: "mobileQuaternaryContent", in: Bundle.module, compatibleWith: nil)! }
+    public var quinaryContent: UIColor { UIColor(named: "mobileQuinaryContent", in: Bundle.module, compatibleWith: nil)! }
+    public var system: UIColor { UIColor(named: "mobileSystem", in: Bundle.module, compatibleWith: nil)! }
+    public var background: UIColor { UIColor(named: "mobileBackground", in: Bundle.module, compatibleWith: nil)! }
+    
+    public let contentAndAvatars: [UIColor] = [
+        .global.azure,
+        .global.grape,
+        .global.verde,
+        .global.polly,
+        .global.melon,
+        .global.aqua,
+        .global.prune,
+        .global.kiwi,
+    ]
+    
+    @objcMembers public class GlobalColors: NSObject {
+        public var azure: UIColor { UIColor(named: "globalAzure", in: Bundle.module, compatibleWith: nil)! }
+        public var kiwi: UIColor { UIColor(named: "globalKiwi", in: Bundle.module, compatibleWith: nil)! }
+        public var grape: UIColor { UIColor(named: "globalGrape", in: Bundle.module, compatibleWith: nil)! }
+        public var verde: UIColor { UIColor(named: "globalVerde", in: Bundle.module, compatibleWith: nil)! }
+        public var polly: UIColor { UIColor(named: "globalPolly", in: Bundle.module, compatibleWith: nil)! }
+        public var melon: UIColor { UIColor(named: "globalMelon", in: Bundle.module, compatibleWith: nil)! }
+        public var elementGreen: UIColor { UIColor(named: "globalElementGreen", in: Bundle.module, compatibleWith: nil)! }
+        public var white: UIColor { UIColor(named: "globalWhite", in: Bundle.module, compatibleWith: nil)! }
+        public var vermillon: UIColor { UIColor(named: "globalVermillon", in: Bundle.module, compatibleWith: nil)! }
+        public var ems: UIColor { UIColor(named: "globalEms", in: Bundle.module, compatibleWith: nil)! }
+        public var aqua: UIColor { UIColor(named: "globalAqua", in: Bundle.module, compatibleWith: nil)! }
+        public var prune: UIColor { UIColor(named: "globalPrune", in: Bundle.module, compatibleWith: nil)! }
+        public var links: UIColor { UIColor(named: "globalLinks", in: Bundle.module, compatibleWith: nil)! }
+        public var grey25: UIColor { UIColor(named: "globalGrey25", in: Bundle.module, compatibleWith: nil)! }
+        public var grey50: UIColor { UIColor(named: "globalGrey50", in: Bundle.module, compatibleWith: nil)! }
+        public var grey100: UIColor { UIColor(named: "globalGrey100", in: Bundle.module, compatibleWith: nil)! }
+        public var grey150: UIColor { UIColor(named: "globalGrey150", in: Bundle.module, compatibleWith: nil)! }
+        public var grey200: UIColor { UIColor(named: "globalGrey200", in: Bundle.module, compatibleWith: nil)! }
+        public var grey250: UIColor { UIColor(named: "globalGrey250", in: Bundle.module, compatibleWith: nil)! }
+        public var grey300: UIColor { UIColor(named: "globalGrey300", in: Bundle.module, compatibleWith: nil)! }
+        public var grey400: UIColor { UIColor(named: "globalGrey400", in: Bundle.module, compatibleWith: nil)! }
+        public var grey450: UIColor { UIColor(named: "globalGrey450", in: Bundle.module, compatibleWith: nil)! }
+        public var black800: UIColor { UIColor(named: "globalBlack800", in: Bundle.module, compatibleWith: nil)! }
+        public var black900: UIColor { UIColor(named: "globalBlack900", in: Bundle.module, compatibleWith: nil)! }
+        public var black950: UIColor { UIColor(named: "globalBlack950", in: Bundle.module, compatibleWith: nil)! }
+        public var ice: UIColor { UIColor(named: "globalIce", in: Bundle.module, compatibleWith: nil)! }
+    }
 }
